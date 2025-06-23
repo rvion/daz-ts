5a2cea00f96579e70df342fe947b9bf3de351029
rvion <vion.remi@gmail.com>
2025-06-23 21:15:45 +0200

    misc

--- src/utils/parseDazUrl.test.ts
+++ src/utils/parseDazUrl.test.ts
@@ -7,6 +7,6 @@ describe('parseDazUrl', () => {
       // found in People/Genesis 9/Genesis 9.duf
       const t1 = '/data/Daz%203D/Genesis%209/Base/Genesis9.dsf#Genesis9-1' as string_DazUrl
 
-      expect(getPathFromDazUrl(t1)).toBe('/data/Daz 3D/Genesis 9/Base/Genesis9.dsf')
+      expect(getPathFromDazUrl(t1)).toBe('data/Daz 3D/Genesis 9/Base/Genesis9.dsf')
    })
 })
